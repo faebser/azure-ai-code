373a37952d5dee4f2ae2f43d9102c54c12d0944d
Lukas Truniger <lukas.truniger@gmail.com>
2021-07-19 18:04:44 +0200

    Actual run script
    added correct paths and parameter

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/bash
-rm /path/to/files/*.wav
-conda activate env
+#!/bin/bash
+rm /home/panorama/azure-ai-code/recordings/*.wav -f
+source  /home/panorama/anaconda3/etc/profile.d/conda.sh
+pulseaudio --start
+conda activate base
+python /home/panorama/vosk-api/python/example/test_microphone.py -l
+cd /home/panorama/azure-ai-code/
 python installation.py
